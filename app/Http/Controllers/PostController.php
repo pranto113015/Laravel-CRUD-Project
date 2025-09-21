111a3f0c640ee20e723317fbf994701d4a6ee1e0
pranto113015 <pranto3876@gmail.com>
2025-09-21 12:34:46 +0600

    implement post creation functionality with success message display

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,8 +11,19 @@ class PostController extends Controller
         return view('create');
     }
 
+
+       //store data
         public function ourfilestore(Request $request){
-            return $request->all();
+
+            $post = new Post;
+
+            $post->name = $request->name;
+            $post->description = $request->description;
+            $post->image = $request->image;
+            $post->save();
+
+            //redirect
+            return redirect()->route('home')->with('success', 'Post created successfully.');
         }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 
 
